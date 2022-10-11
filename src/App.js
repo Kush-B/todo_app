14901198e1e6473b8ab3883a4aa3e0b7a5cdf77b
Kush Bhardwaj <bhardwajkush01@gmail.com>
2022-10-11 17:07:32 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, This is the ToDo List!</h1>
+      <input />
+      <button>Add ToDo</button>
+
+      <ul>
+        <li>Take dogs for walk</li>
+        <li>Do the dishes</li>
+      </ul>
     </div>
   );
 }
